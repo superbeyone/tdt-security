d490f2da435150b0565412d5db17a786f0273b9b
superbeyone <superbeyone@outlook.com>
2018-11-24 22:32:50 +0800

    project init

--- tdt-security-apply/src/main/java/com/tdt/security/TdtSecurityApplyApplication.java
+++ tdt-security-apply/src/main/java/com/tdt/security/TdtSecurityApplyApplication.java
@@ -1,12 +1,22 @@
-package com.tdt.security.apply;
+package com.tdt.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Project: tdt-security
+ * @ClassName: TdtSecurityApplyApplication
+ * @Author: Mr.superbeyone
+ * @Time: 2018-11-24 16:21
+ * @Description: SecurityApply
+ **/
+
 @SpringBootApplication
 public class TdtSecurityApplyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TdtSecurityApplyApplication.class, args);
     }
+
 }
+
+
