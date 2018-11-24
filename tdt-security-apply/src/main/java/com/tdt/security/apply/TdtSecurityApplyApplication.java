7c384de91b1e9a8294dc80ed19e9006562d4e5b7
superbeyone <superbeyone@outlook.com>
2018-11-24 16:08:42 +0800

    tdt-security-init

---
+++ tdt-security-apply/src/main/java/com/tdt/security/apply/TdtSecurityApplyApplication.java
@@ -0,0 +1,12 @@
+package com.tdt.security.apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdtSecurityApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdtSecurityApplyApplication.class, args);
+    }
+}
---
+++ tdt-security-browser/src/main/java/com/tdt/security/browser/TdtSecurityBrowserApplication.java
@@ -0,0 +1,12 @@
+package com.tdt.security.browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdtSecurityBrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdtSecurityBrowserApplication.class, args);
+    }
+}
